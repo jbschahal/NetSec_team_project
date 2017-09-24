5c619a4d2cc876d49c5fbde12baa0025263bc5d4
chengsteven <stevencheng@jhu.edu>
2017-09-24 19:21:55 -0400

    protocol packets

---
+++ lab2/peep_packets.py
@@ -0,0 +1,16 @@
+from playground.network.packet import PacketType, FIELD_NOT_SET
+from playground.network.packet.fieldtypes import UINT8, UINT16, UINT32, UINT64,\
+    STRING, BUFFER, BOOL, ComplexFieldType, PacketFields
+from playground.network.packet.fieldtypes.attributes import Optional
+
+class PEEP_Packet(PacketType):
+    DEFINITION_IDENTIFIER = "PEEP-Packet"
+    DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+        ("Type", UINT8),
+        ("SequenceNumber", UINT32({Optional: "True"})),
+        ("Checksum", UINT16),
+        ("Acknowledgement", UINT32({Optional: True})),
+        ("Data", STRING({Optional: True}))
+    ]
