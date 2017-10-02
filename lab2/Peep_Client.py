27a3fc92790088f39a9ff80df0185ece7a3f8f04
Suyi Liu <sliu92@jhu.edu>
2017-10-02 15:32:07 -0400

    resolved conflicts

--- lab2/Peep_Client.py
+++ lab2/Peep_Client.py
@@ -1,50 +1,18 @@
 import asyncio
 import random
+import playground
 from Peep_Packets import PEEP_Packet
 from playground.network.packet import PacketType
+from PassingThroughProtocols import FirstPassingThroughProtocol, SecondPassingThroughProtocol
 from playground.network.common import StackingProtocol, StackingTransport, StackingProtocolFactory
 
-
-class PEEP_Client(asyncio.Protocol):
-
-    def __init__(self):
-        self.transport = None
-        self.deserializer = None
-
-    def data_received(self, data):
-        self.deserializer.update(data)
-        for packet in self.deserializer.nextPackets():
-            if isinstance(packet, PEEP_Packet):
-                print("Type of packet:")
-                print(packet.Type)
-
-    def connection_made(self, transport):
-        print("client connection made")
-        self.deserializer = PacketType.Deserializer()
-        self.transport = transport
-        #self.start_handshake()
-
-    def connection_lost(self):
-        self.transport = None
-
-class PEEP_Passthrough2(StackingProtocol):
-    def __init__(self):
-        self.transport = None
-
-    def connection_made(self, transport):
-        self.transport = transport
-        print("\nConnection made to the client middle layer\n")
-        print(self.higherProtocol())
-        self.higherProtocol().connection_made(StackingTransport(transport))
-
-    def data_received(self, data):
-        self.higherProtocol().data_received(data)
-
-class PEEP_Passthrough1(StackingProtocol):
+class PEEP_Client(StackingProtocol):
     def __init__(self):
         self.transport = None
         self.state = 0
         self.deserializer = None
+        self.higherProtocolConnectionMade = False
+        super().__init__
 
     def data_received(self, data):
         if self.state == 1:
@@ -55,10 +23,12 @@ class PEEP_Passthrough1(StackingProtocol):
                     if (packet.Type == 1):
                         # received a synack
                         if (packet.verifyChecksum()):
-                            ##Check for wrong packets -------
+                            higherTransport = StackingTransport(self.transport)
+                            self.higherProtocol().connection_made(higherTransport)
+                            self.higherProtocolConnectionMade = True
+
                             if packet.Acknowledgement == self.sequence_number + 1:
                                 print("Received synack")
-                                self.higherProtocol().connection_made(StackingTransport(self.transport))
                                 print("connection_made to higher protocol: Middle layer")
                                 print("Sending Back Ack")
                                 packet_to_send = PEEP_Packet()
@@ -75,11 +45,16 @@ class PEEP_Passthrough1(StackingProtocol):
 
     def connection_made(self, transport):
         self.transport = transport
-        elf.deserializer = PacketType.Deserializer()
-        print("\nConnection made to lower client passthrough 1\n")
-        #print(self.higherProtocol())
-        #self.higherProtocol().connection_made(StackingTransport(transport))
-    
+        self.deserializer = PacketType.Deserializer()
+        self.start_handshake()
+        if self.higherProtocolConnectionMade == True:
+            self.higherProtocol().data_received(data)
+
+    def connection_lost(self, exc):
+        self.transport = None 
+        if self.higherProtocolConnectionMade == True:
+            self.higherProtocol().connection_lost(exc)
+
     def start_handshake(self):
         self.sequence_number = random.randint(0,1000)
         print("Client start handshake")
@@ -89,3 +64,13 @@ class PEEP_Passthrough1(StackingProtocol):
         packet.updateChecksum()
         self.transport.write(packet.__serialize__())
         self.state = 1
+
+loop = asyncio.get_event_loop()
+loop.set_debug(enabled=True)
+# Chains two layers together
+f = StackingProtocolFactory(lambda: FirstPassingThroughProtocol(), lambda: PEEP_Client())
+ptConnector = playground.Connector(protocolStack=f)
+playground.setConnector("passthrough", ptConnector)
+coro = playground.getConnector('passthrough').create_playground_connection(lambda: SecondPassingThroughProtocol(), '20174.1.1.1', 888)
+transport, c = loop.run_until_complete(coro)
+loop.run_forever()
--- lab2/Peep_Server.py
+++ lab2/Peep_Server.py
@@ -1,21 +1,28 @@
 import asyncio
+import playground
 from playground.network.packet import PacketType
 from playground.network.packet.fieldtypes import UINT32, STRING
 from Peep_Packets import PEEP_Packet
 from random import randint
+from PassingThroughProtocols import FirstPassingThroughProtocol, SecondPassingThroughProtocol
+from playground.network.common import StackingProtocol, StackingTransport, StackingProtocolFactory
 
-class PEEP_Server(asyncio.Protocol):
+class PEEP_Server(StackingProtocol):
     def __init__(self):
         self.received=0
         self.transport=None
         self._Deserializer = PacketType.Deserializer()
         self.state=0
+        self.higherProtocolConnectionMade = False
+        super().__init__
 
     def connection_made(self,transport):
         self.transport=transport
         self._Deserializer=PacketType.Deserializer()
         peername = transport.get_extra_info('peername')
         print('server(prepare)-->client(prepare):Connection from {}'.format(peername))
+        if self.higherProtocolConnectionMade == True:
+            self.higherProtocol().data_received(data)
 
     def data_received(self,data):
         self._Deserializer=PacketType.Deserializer()
@@ -42,14 +49,15 @@ class PEEP_Server(asyncio.Protocol):
                 print('server side: received UNKNOWN TYPE')
         else:
             print('server side:This packet is not the correct type')
+
     def handle_syn(self,pkt):
-        if pkt.Checksum==0:
+        if pkt.verifyChecksum():
             print('server side: checksum of SYN is correct')
             pktback=PEEP_Packet()
             pktback.Acknowledgement=pkt.SequenceNumber+1
             pktback.SequenceNumber = randint(0,100)
             pktback.Type=1
-            pktback.Checksum=0
+            pktback.updateChecksum()
             self.transport.write(pktback.__serialize__())
             self.state+=1
             print('server side: sent SYNACK')
@@ -57,41 +65,43 @@ class PEEP_Server(asyncio.Protocol):
             print('server side: checksum of SYN is incorrect')
             pktback=PEEP_Packet()
             pktback.Type=5
-            pktback.Checksum=0
+            pktback.updateChecksum()
             self.transport.write(pktback.__serialize__())
             print('server side: sent RST')
             # clear buffer
             self.transport.close()
 
     def handle_ack(self,pkt):
-        if pkt.Checksum==0:
+        if pkt.verifyChecksum():
             print('server side: checksum of ACK is correct')
             #send data
             self.state+=1
-
+            higherTransport = StackingTransport(self.transport)
+            self.higherProtocol().connection_made(higherTransport)
+            self.higherProtocolConnectionMade = True
         else:
             print('server side: checksum of ACK is incorrect')
             pktback=PEEP_Packet()
             pktback.Type=5
-            pktback.Checksum=0
+            pktback.updateChecksum()
             self.transport.write(pktback.__serialize__())
             self.transport.close()
             print('server side: sent RST')
 
     def handle_rip(self,pkt):
-        if pkt.Checksum==0:
+        if pkt.verifyChecksum():
             print('server side: checksum of RIP is correct')
             pktback=PEEP_Packet()
             pktback.Acknowledgement=pkt.SequenceNumber+1
             pktback.Type=4
-            pktback.Checksum=0
+            pktback.updateChecksum()
             self.transport.write(pktback.__serialize__())
             print('server side: sent RIPACK')
             # Sending remaining packets back
             pktback2=PEEP_Packet()
             pktback2.Acknowledgement=pkt.SequenceNumber+1
             pktback2.Type=3
-            pktback2.Checksum=0
+            pktback2.updateChecksum()
             pktback2.SequenceNumber=randint(0,100)
             self.transport.write(pktback2.__serialize__())
             print('server side: sent RIP')
@@ -99,16 +109,30 @@ class PEEP_Server(asyncio.Protocol):
             print('server side: checksum of RIP is incorrect')
             pktback=PEEP_Packet()
             pktback.Type=5
-            pktback.Checksum=0
+            pktback.updateChecksum()
             self.transport.write(pktback.__serialize__())
+            self.higherProtocol().transport.close()
             self.transport.close()
             print('server side: sent RST')
 
     def handle_ripack(self,pkt):
-        if pkt.Checksum==0:
+        if pkt.verifyChecksum():
             print('server side: checksum of RIPACK is correct')
+            self.higherProtocol().transport.close()
             self.transport.close()
         else:
             print('server side: checksum of RIPACK is incorrect')
+            self.higherProtocol().transport.close()
             self.transport.close()
 
+loop = asyncio.get_event_loop()
+loop.set_debug(enabled=True)
+# Chains two layers together
+f = StackingProtocolFactory(lambda: FirstPassingThroughProtocol(), lambda: PEEP_Server())
+ptConnector = playground.Connector(protocolStack=f)
+playground.setConnector("passthrough", ptConnector)
+coro = playground.getConnector('passthrough').create_playground_server(lambda: SecondPassingThroughProtocol(), 888)
+server = loop.run_until_complete(coro)
+print('Server now serving')
+loop.run_forever()
+loop.close()
